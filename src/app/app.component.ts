c5dfb5c6c2acc6ab9ea3e5946209be8695311c8c
varshan-repo <varshancog@gmail.com>
2024-06-24 18:21:52 +0530

    added task component and displayed user specific task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UsersComponent } from './users/users.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [HeaderComponent, UsersComponent],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HeaderComponent, UsersComponent, TasksComponent]
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId!: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onselectUser(id: string){
+    this.selectedUserId = id;
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { User } from './users.model';
 @Component({
@@ -6,15 +6,17 @@ import { User } from './users.model';
   standalone: true,
   imports: [],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
-
 export class UsersComponent {
-
   @Input({ required: true }) user!: User;
-
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
